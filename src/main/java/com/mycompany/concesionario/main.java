d3ca4a7a632e09c74a727370af37fd4c0e45f8d7
Arjun <arjunjot18@gmail.com>
2022-11-01 20:14:01 +0100

    q

---
+++ src/main/java/com/mycompany/concesionario/main.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.concesionario;    
+
+public class main {
+
+    public static void main(String[] args) {
+        Concesionario v = new Concesionario();
+        v.setVisible(true);
+        
+    }
+}
